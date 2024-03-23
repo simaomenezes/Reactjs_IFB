127f39def84f2c2956700c4379a2009693179c26
Simão Neto <simaomenezesneto@gmail.com>
2024-03-23 11:09:34 +0000

    chore: Layouts da aplicação, applayout e authlayout

--- pizzashop-web/src/routes.tsx
+++ pizzashop-web/src/routes.tsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./pages/app/dashboard";
 import { SingIn } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 
 export const router = createBrowserRouter([
-    {path: '/', element: <Dashboard/>},
-    {path: '/sing-in', element: <SingIn/>},
+{
+    path:'/',
+    element: <AppLayout/>,
+    children:[    {path: '/', element: <Dashboard/>},]
+},
+{
+    path:'/',
+    element: <AuthLayout/>,
+    children:[    {path: '/sing-in', element: <SingIn/>},]
+}
 ])

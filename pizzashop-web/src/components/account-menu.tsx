36adbc7944a03ad239242c827ddc4c5a2e4247fc
Simão Neto <simaomenezesneto@gmail.com>
2024-07-19 11:47:46 +0100

    feat: showed and add profile at home

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -1,8 +1,21 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccountMenu(){
+    const { data : profile } = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile,
+    })
+
+    const { data: managedRestaurant } = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant,        
+    })
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -10,14 +23,14 @@ export function AccountMenu(){
                     variant="outline"
                     className="flex select-none items-center gap-2"
                 >
-                    Pizza Shop
+                    {managedRestaurant?.name}
                     <ChevronDown className="h-4 w-4"/>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel className="flex flex-col">
-                    <span>Simão Neto</span>
-                    <span>simaomenezesneto@gmail.com</span>
+                    <span>{profile?.name}</span>
+                    <span>{profile?.email}</span>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator/>
                 <DropdownMenuItem>

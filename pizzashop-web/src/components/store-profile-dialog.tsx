17d19d90696d157db976a3c8357e50b4206757e6
Simão Neto <simaomenezesneto@gmail.com>
2024-09-02 22:34:37 +0100

    feat: update HTTP state

--- pizzashop-web/src/components/store-profile-dialog.tsx
+++ pizzashop-web/src/components/store-profile-dialog.tsx
@@ -1,12 +1,12 @@
-import { useMutation, useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { Button } from "./ui/button"
 import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog"
 import { Input } from "./ui/input"
 import { Label } from "./ui/label"
 import { Textarea } from "./ui/textarea"
-import { getManagedRestaurant } from "@/api/get-managed-restaurant"
+import { getManagedRestaurant, GetManagedRestaurantResponse } from "@/api/get-managed-restaurant"
 import { zodResolver } from '@hookform/resolvers/zod'
-import { date, z } from "zod"
+import { z } from "zod"
 import { useForm } from "react-hook-form"
 import { updateProfile } from "@/api/update-profile"
 import { toast } from "sonner"
@@ -21,6 +21,7 @@ const storeProfileSchema = z.object({
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
 
 export function StoreProfileDialog(){
+    const queryClient = useQueryClient()
 
     const { data: managedRestaurant } = useQuery({
         queryKey: ['managed-restaurant'],
@@ -42,11 +43,21 @@ export function StoreProfileDialog(){
 
     const { mutateAsync: updateProfileFn } = useMutation({
         mutationFn: updateProfile,
+        onSuccess(_, { name, description })
+        {
+            const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant',])          
+            if(cached){
+                queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'],{
+                    ...cached,
+                    name,
+                    description,
+                })
+            }
+        },
     })
 
     async function handleUpdateProfile(data: StoreProfileSchema){
         try {
-            console.log(data.name)
             await updateProfileFn({
                 name: data.name,
                 description: data.description,

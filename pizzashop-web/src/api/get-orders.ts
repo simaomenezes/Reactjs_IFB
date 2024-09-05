81e854a60da2f464ed44357bedd9c52c2cf10ae0
Simão Neto <simaomenezesneto@gmail.com>
2024-09-05 22:21:45 +0100

    feat: Maked request of filter - list products

--- pizzashop-web/src/api/get-orders.ts
+++ pizzashop-web/src/api/get-orders.ts
@@ -1,30 +1,41 @@
-import { api } from "@/lib/axios";
+import { api } from '@/lib/axios'
 
 export interface GetOrdersQuery {
-    pageIndex?: number | null
+  pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
 }
 
 export interface GetOrdersResponse {
-    orders: {
-        orderId: string;
-        createdAt: Date;
-        status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
-        customerName: string;
-        total: number;
-    }[];
-    meta: {
-        pageIndex: number;
-        perPage: number;
-        totalCount: number;
-    };
+  orders: {
+    orderId: string
+    createdAt: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    customerName: string
+    total: number
+  }[]
+  meta: {
+    pageIndex: number
+    perPage: number
+    totalCount: number
+  }
 }
 
-export async function getOrders({ pageIndex }: GetOrdersQuery){
-    const response = await api.get<GetOrdersResponse>('/orders', {
-        params: {
-            pageIndex,
-        },
-    })
+export async function getOrders({
+  pageIndex,
+  customerName,
+  orderId,
+  status,
+}: GetOrdersQuery) {
+  const response = await api.get<GetOrdersResponse>('/orders', {
+    params: {
+      pageIndex,
+      orderId,
+      customerName,
+      status,
+    },
+  })
 
-    return response.data
+  return response.data
 }
